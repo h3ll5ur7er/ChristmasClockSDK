069ba925404e794c860f7471645f0356dfbe5be5
molitoris <rafa.molitoris@gmail.com>
2022-12-21 06:17:24 +0000

    feat: Move ir receiver into ChristmasClock

--- app/src/App.cpp
+++ app/src/App.cpp
@@ -1,45 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#include "AppHdr.hpp"
-
-#include "receiver.hpp"
-#include "nec_message.hpp"
-
-
-void ChristmasClock::ChristmasClock::Init() {
-
-    // ir receiver setup
-    cont uint recInputPin = 13;     // gpio input pin
-    PIO pio = pio0;                 // programable io
-    Receiver rec;                   // ir receiver
-    NEC_Message message;            // ir message
-
-    if(rec.init(pio, recInputPin)) {
-        printf("Could not initialize IR receiver\n");
-        return;
-    }    
-
-    // main loop
-    while(true){
-
-        // Check for new singal from ir receiver
-        rec.update(&message);
-
-        // Process ir signal
-        if(message.isNew() && message.isValid()){
-            printf("Got new and valid ir message (data: %s)", message.getData());
-            message.markAsOld();
-        } else {
-            printf("No new valid message received");
-        }
-    }
-}
+#include "ChristmasClock.hpp"
 
 int main() {
     stdio_init_all(); 
 
     ChristmasClock::ChristmasClock clock;
-    clock.Init();
-    return 0;
+    const int state = clock.init();
+    if(state != 0) {
+        printf("Could not initialize clock\n");
+        return state;
+    }
+    
+    // main loop
+    while(true){
+        clock.update();
+    }
+    return -1;
 }
--- drivers/ir/README.md
+++ drivers/ir/README.md
@@ -8,3 +8,5 @@ The ir receiver is implemented on the PIO and supports the NEC protocol.
 
 ![NEC message frame](../../res/image/NECMessageFrame.png)
 
+Limitations: NEC repeat codes are not supported.
+
